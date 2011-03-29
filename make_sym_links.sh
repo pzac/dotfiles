a6cb76fd05aae7a426dff6d02239d47ec8846798
Paolo Zaccagnini <paozac@gmail.com>
2011-03-29 13:37:25 +0200

    added rvmrc

--- make_sym_links.sh
+++ make_sym_links.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-ln -s ~/dotfiles/irbrc ~/.irbrc
-ln -s ~/dotfiles/editrc ~/.editrc
+ln -sf ~/dotfiles/irbrc ~/.irbrc
+ln -sf ~/dotfiles/editrc ~/.editrc
+ln -sf ~/dotfiles/rvmrc ~/.rvmrc
 
