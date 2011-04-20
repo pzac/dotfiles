6ba69a85d7ca37eda9a4a90b48d87e2332d838f5
Paolo Zaccagnini <paozac@gmail.com>
2011-04-20 09:28:46 +0200

    updated gitconfig and install script

--- make_sym_links.sh
+++ make_sym_links.sh
@@ -2,4 +2,6 @@
 ln -sf ~/dotfiles/irbrc ~/.irbrc
 ln -sf ~/dotfiles/editrc ~/.editrc
 ln -sf ~/dotfiles/rvmrc ~/.rvmrc
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+echo "Remember to edit your git user name and email"
 
