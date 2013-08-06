1fbb65b2364e3ac6609076156f06cc33b7be4498
Paolo Zaccagnini <paozac@gmail.com>
2013-08-06 16:00:02 +0100

    Use .dotfiles as standard directory

--- make_sym_links.sh
+++ make_sym_links.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-ln -sf ~/dotfiles/irbrc ~/.irbrc
-ln -sf ~/dotfiles/editrc ~/.editrc
-ln -sf ~/dotfiles/bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/gemrc ~/.gemrc
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/zshenv ~/.zshenv
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/ackrc ~/.ackrc
-ln -sf ~/dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/irbrc ~/.irbrc
+ln -sf ~/.dotfiles/editrc ~/.editrc
+ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/gemrc ~/.gemrc
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/zshenv ~/.zshenv
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/ackrc ~/.ackrc
+ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 
