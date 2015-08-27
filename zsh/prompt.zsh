f9bc293aaa4c4cd538b412dd0dd6d443fe250268
Paolo Zaccagnini <paolo@pzac.net>
2015-08-27 10:42:37 +0100

    Prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,18 @@
-export PROMPT='%F{033}%~ %F{030}$(git_prompt_string) %F{226}» %{$reset_color%}'
+export PROMPT='%F{033}%~ %F{030}$(git_prompt_string) %F{226}» %{$RESET%}'
 # %T = HH:MM
 # %* = HH:MM:SS
-export RPROMPT='%F{111}%*%{$reset_color%}'
+# export RPROMPT='%F{111}%*%{$RESET%}'
+
+# see https://coderwall.com/p/kmchbw/zsh-display-commands-runtime-in-prompt
+#
+function preexec() {
+  timer=${timer:-$SECONDS}
+}
+
+function precmd() {
+  if [ $timer ]; then
+    timer_show=$(($SECONDS - $timer))
+    export RPROMPT="%F{cyan}${timer_show}s %{$reset_color%}"
+    unset timer
+  fi
+}
