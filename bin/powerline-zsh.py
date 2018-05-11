f6aa6bec78a20f68bc0775e414fc66791e498226
Paolo Zaccagnini <paolo@pzac.net>
2018-05-11 18:33:10 +0200

    Fix powerline.zsh

--- bin/powerline-zsh.py
+++ bin/powerline-zsh.py
@@ -143,7 +143,7 @@ def add_git_segment(powerline, cwd):
     p = subprocess.Popen(['git', 'symbolic-ref', '-q', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
 
-    if 'Not a git repo' in err.decode(encoding):
+    if 'not a git repo' in err.decode(encoding):
         return False
 
     if out:
