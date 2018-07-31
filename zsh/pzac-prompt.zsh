bf89731d280611aa275bd5fe554fc05d2dacdd15
Paolo Zaccagnini <paolo@pzac.net>
2018-07-31 08:49:40 +0200

    ...

--- zsh/pzac-prompt.zsh
+++ zsh/pzac-prompt.zsh
@@ -1,4 +1,6 @@
 source $HOME/.dotfiles/zsh/git-prompt.zsh
 # source $HOME/.dotfiles/zsh/prompt-command-duration.zsh
 
-export PROMPT='%F{033}%~ %F{030}$(git_prompt_string) %F{226}» %{$RESET%}'
+# export PROMPT='%F{033}%~ %F{030}$(git_prompt_string) %F{226}» %{$RESET%}'
+# export PROMPT='%F{027}%~ %F{040}$(git_prompt_string) %F{191}» %{$RESET%}'
+export PROMPT='%F{196}%~ %F{214}$(git_prompt_string) %F{226}» %{$RESET%}'
