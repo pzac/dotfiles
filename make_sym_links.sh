e25a40f8c8269768f6b76fca7a9023ae67e8831f
Paolo Zaccagnini <paolo@pzac.net>
2020-06-05 08:48:41 +0200

    Sublime Merge theme tweak

--- make_sym_links.sh
+++ make_sym_links.sh
@@ -14,6 +14,7 @@ ln -sf ~/.dotfiles/Preferences.sublime-settings  ~/Library/Application\ Support/
 ln -sf ~/.dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 ln -sf ~/.dotfiles/sublime/bp.sublime-snippet  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 ln -sf ~/.dotfiles/sublime/better-do-end.sublime-snippet  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+ln -sf ~/.dotfiles/sublime/Merge.sublime-theme  ~/Library/Application\ Support/Sublime\ Merge/Packages/User/
 ln -sf ~/.dotfiles/psqlrc ~/.psqlrc
 ln -sf ~/.dotfiles/bin ~/bin
 ln -sf ~/.dotfiles/pryrc ~/.pryrc
