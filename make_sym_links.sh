d3acb651842e4e32588a95e29d6ba7822fd283ad
Paolo Zaccagnini <paolo@pzac.net>
2020-11-16 17:30:36 +0100

    Separate mac-specific links

--- make_sym_links.sh
+++ make_sym_links.sh
@@ -10,11 +10,15 @@ ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/ackrc ~/.ackrc
 ln -sf ~/.dotfiles/vim ~/.vim
-ln -sf ~/.dotfiles/Preferences.sublime-settings  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-ln -sf ~/.dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-ln -sf ~/.dotfiles/sublime/bp.sublime-snippet  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-ln -sf ~/.dotfiles/sublime/better-do-end.sublime-snippet  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-ln -sf ~/.dotfiles/sublime/Merge.sublime-theme  ~/Library/Application\ Support/Sublime\ Merge/Packages/User/
 ln -sf ~/.dotfiles/psqlrc ~/.psqlrc
 ln -sf ~/.dotfiles/bin ~/bin
 ln -sf ~/.dotfiles/pryrc ~/.pryrc
+
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  ln -sf ~/.dotfiles/Preferences.sublime-settings  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+  ln -sf ~/.dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+  ln -sf ~/.dotfiles/sublime/bp.sublime-snippet  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+  ln -sf ~/.dotfiles/sublime/better-do-end.sublime-snippet  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+  ln -sf ~/.dotfiles/sublime/Merge.sublime-theme  ~/Library/Application\ Support/Sublime\ Merge/Packages/User/
+fi
