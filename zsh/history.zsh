f8e53f6f73a5f5d1eb45c09026bb4e47f2a1ce21
Paolo Zaccagnini <paolo@pzac.net>
2022-08-09 10:32:00 +0200

    Zsh history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -7,12 +7,13 @@ fi
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt append_history
-setopt extended_history
-setopt hist_expire_dups_first
-setopt hist_ignore_dups # ignore duplication command history list
-setopt hist_ignore_space
-setopt hist_verify
 setopt inc_append_history
-setopt share_history # share command history data
-
+setopt append_history # Allow multiple terminal sessions to all append to one zsh command history
+# setopt extended_history # save timestamp of command and duration
+setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
+setopt hist_ignore_dups # Do not write events to history that are duplicates of previous events
+setopt hist_ignore_space # remove command line from history list when first character on the line is a space
+setopt hist_find_no_dups # When searching history don't display results already cycled through twice
+setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
+setopt hist_verify # don't execute, just expand history
+setopt share_history # imports new commands and appends typed commands to history
--- zsh/options.zsh
+++ zsh/options.zsh
@@ -8,16 +8,6 @@ unsetopt nomatch # If a pattern for filename generation has no matches, leave it
 setopt no_case_glob  # set ignore case for ls etc
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-setopt append_history # Allow multiple terminal sessions to all append to one zsh command history
-setopt extended_history # save timestamp of command and duration
-setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
-setopt hist_ignore_dups # Do not write events to history that are duplicates of previous events
-setopt hist_ignore_space # remove command line from history list when first character on the line is a space
-setopt hist_find_no_dups # When searching history don't display results already cycled through twice
-setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
-setopt hist_verify # don't execute, just expand history
-setopt share_history # imports new commands and appends typed commands to history
-
 setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
 setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
 setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
@@ -29,4 +19,4 @@ unsetopt correct_all
 setopt prompt_subst # Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
 # setopt transient_rprompt # only show the rprompt on the current prompt
 
-cdpath=(~/Projects)
+cdpath=(~/code ~/code/ifad)
