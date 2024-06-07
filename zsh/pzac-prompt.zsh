05678102555401bb50c83bc7beed0f55b9a23719
Paolo Zaccagnini <paolo.zaccagnini@doctolib.com>
2024-06-07 14:04:58 +0200

    My version of powerline

--- zsh/pzac-prompt.zsh
+++ zsh/pzac-prompt.zsh
@@ -1,6 +1,14 @@
 source $DOTFILES_HOME/zsh/git-prompt.zsh
 
-_PROMPT='%F{196}%~ %F{214}$(git_prompt_string) %F{226}» %{$RESET%}'
+POWERLINE=1
+
+RESET="$reset_color"
+
+if [[ "$POWERLINE" == 1 ]]; then
+  _PROMPT=$'%K{190}%F{22} %~ %F{190}%K{22}\uE0B0 %F{190} $(git_prompt_string) %{$RESET%}%F{22}\uE0B0 %{$RESET%}'
+else
+  _PROMPT='%F{196}%~ %F{214}$(git_prompt_string) %F{226}» %{$RESET%}'
+fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export PROMPT="$_PROMPT"
