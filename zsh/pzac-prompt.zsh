790bc5d213710f7c1313a6debff59b7e6cb7c859
Paolo Zaccagnini <paolo.zaccagnini@doctolib.com>
2024-06-07 08:43:28 +0200

    Simplify prompt

--- zsh/pzac-prompt.zsh
+++ zsh/pzac-prompt.zsh
@@ -1,7 +1,9 @@
 source $DOTFILES_HOME/zsh/git-prompt.zsh
 
+_PROMPT='%F{196}%~ %F{214}$(git_prompt_string) %F{226}» %{$RESET%}'
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  export PROMPT='%F{196}%~ %F{214}$(git_prompt_string) %F{226}» %{$RESET%}'
+  export PROMPT="$_PROMPT"
 else
-  export PROMPT='[%n@%m] %F{196}%~ %F{214}$(git_prompt_string) %F{226}%# %{$RESET%}'
+  export PROMPT="[%n@%m] $_PROMPT"
 fi
