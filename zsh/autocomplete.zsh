499c74d53964dd415065c18d7e1cb988527c89f8
Paolo Zaccagnini <paolo@pzac.net>
2024-12-17 08:52:28 +0100

    Simplify zsh config

--- zsh/autocomplete.zsh
+++ zsh/autocomplete.zsh
@@ -1,51 +1,59 @@
 # Mostly taken from oh-my-zsh
+
+# initialize completion for the current session
 autoload -U compinit && compinit
 
-# fixme - the load process here seems a bit bizarre
+setopt always_to_end # Always place the cursor to the end of the word completed
+setopt auto_menu # show completion menu on succesive tab press. needs unsetop menu_complete to work
+unsetopt menu_complete # do not autoselect the first completion entry
+setopt complete_in_word # Allow completion from within a word/phrase
 
-unsetopt menu_complete   # do not autoselect the first completion entry
-unsetopt flowcontrol
-setopt auto_menu         # show completion menu on succesive tab press
-setopt complete_in_word
-setopt always_to_end
 
-WORDCHARS=''
+WORDCHARS='' # string of characters which are also part of a word
 
+# The zsh/complist module offers three extensions to completion listings: the ability
+# to highlight matches in such a list, the ability to scroll through long lists and a
+# different style of menu completion.
 zmodload -i zsh/complist
 
-## case-insensitive (all),partial-word and then substring completion
-if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  unset CASE_SENSITIVE
-else
-  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-fi
-
-zstyle ':completion:*' list-colors ''
-
-# should this be in keybindings?
-bindkey -M menuselect '^o' accept-and-infer-next-history
-
-zstyle ':completion:*:*:*:*:*' menu select
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
-
-# disable named-directories autocompletion
-zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
 # Use caching so that commands like apt and dpkg complete are useable
 zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion::complete:*' cache-path $ZSH/cache/
 
-# ... unless we really want to.
-zstyle '*' single-ignored show
 
-if [ "x$COMPLETION_WAITING_DOTS" = "xtrue" ]; then
-  expand-or-complete-with-dots() {
-    echo -n "\e[31m......\e[0m"
-    zle expand-or-complete
-    zle redisplay
-  }
-  zle -N expand-or-complete-with-dots
-  bindkey "^I" expand-or-complete-with-dots
-fi
+
+# Stuff we can probaby do without
+# unsetopt flowcontrol
+# setopt complete_in_word
+
+# should this be in keybindings?
+# bindkey -M menuselect '^o' accept-and-infer-next-history
+
+
+## case-insensitive (all),partial-word and then substring completion
+# if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
+#   zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+#   unset CASE_SENSITIVE
+# else
+#   zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# fi
+
+# zstyle ':completion:*' list-colors ''
+# zstyle ':completion:*:*:*:*:*' menu select
+# zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+# zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
+# disable named-directories autocompletion
+# zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
+# zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# if [ "x$COMPLETION_WAITING_DOTS" = "xtrue" ]; then
+#   expand-or-complete-with-dots() {
+#     echo -n "\e[31m......\e[0m"
+#     zle expand-or-complete
+#     zle redisplay
+#   }
+#   zle -N expand-or-complete-with-dots
+#   bindkey "^I" expand-or-complete-with-dots
+# fi
--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,22 +1,18 @@
 setopt interactive_comments # Allow comments in interactive shells
+
 setopt auto_cd
+cdpath=(~ ~/code)
 
+
+# Globbing
 setopt extended_glob # treat #, ~, and ^ as part of patterns for filename generation
-
 unsetopt nomatch # If a pattern for filename generation has no matches, leave it unchanged in the argument list
 
 setopt no_case_glob  # set ignore case for ls etc
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
-setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
-setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
-setopt complete_in_word # Allow completion from within a word/phrase
-unsetopt menu_complete # do not autoselect the first completion entry
-
-unsetopt correct_all
+unsetopt correct_all # Do not try to correct the spelling of all arguments in a line.
 
 setopt prompt_subst # Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
-# setopt transient_rprompt # only show the rprompt on the current prompt
 
-cdpath=(~ ~/code)
+# ???
+setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
