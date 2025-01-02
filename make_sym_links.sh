301f084ed0758c89bda346510f2cec21950ffefe
Paolo Zaccagnini <paolo@pzac.net>
2025-01-02 09:30:01 +0100

    Zed settings

--- make_sym_links.sh
+++ make_sym_links.sh
@@ -24,6 +24,10 @@ ln -sf "${HERE}/ghostty/config" ~/.config/ghostty/config
 mkdir -p ~/.config/atuin
 ln -sf "${HERE}/atuin/config.toml" ~/.config/atuin/config.toml
 
+mkdir -p ~/.config/zed
+ln -sf "${HERE}/zed/settings.json" ~/.config/zed/settings.json
+ln -sf "${HERE}/zed/snippets" ~/.config/zed/snippets
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   ln -sf "${HERE}/Preferences.sublime-settings"  ~/Library/Application\ Support/Sublime\ Text/Packages/User/
   ln -sf "${HERE}/sublime/Merge.sublime-theme"  ~/Library/Application\ Support/Sublime\ Merge/Packages/User/
