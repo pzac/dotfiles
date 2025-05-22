8cc284dbfdfb4c29d0cce43afffc80f956c987e2
Paolo Zaccagnini <paolo@pzac.net>
2025-05-22 11:21:35 +0200

    ZSH: colors and arrow selection with autocomplete

--- zsh/autocomplete.zsh
+++ zsh/autocomplete.zsh
@@ -44,7 +44,7 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 #   zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 # fi
 
-# zstyle ':completion:*' list-colors ''
-# zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*:*:*:*:*' menu select
 
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
